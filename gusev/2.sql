bc68434d4dda4a0ef154dd8d288f148fafc74042
Roman <grdesu@gmail.com>
2015-12-25 11:09:19 +0700

    Delete 2.sql

--- gusev/2.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE VIEW another_dispatcher_view
-  AS SELECT contracts.*, users.firstname, users.lastname
-  FROM contracts
-  JOIN users ON contracts.USERID = users.USERID;
-  
-GRANT SELECT ON another_dispatcher_view TO dispatcher;
