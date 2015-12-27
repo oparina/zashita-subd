1023fc72de32a618a882309defa24f4be257f0b8
1 <grdesu@gmail.com>
2015-12-27 15:56:16 +0600

    Begin work on postgres labs

---
+++ gusev/lab3/1.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW client_view
+  AS SELECT (calls.*)
+  FROM calls
+  JOIN contracts ON (contracts.id = calls.caller_id or contracts.id = calls.receiver_id)
+  JOIN users ON users.id = contracts.user_id
+  WHERE users.user_name = (SELECT current_user);
+
+CREATE ROLE client;
+GRANT SELECT ON TABLE client_view TO client;
+CREATE USER john PASSWORD 'password' IN ROLE client;
+
